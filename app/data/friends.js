9b2b5f58501e5ec4d4d995d24fadcefa4619e8c9
DJ <konobeat@gmail.com>
2018-08-12 16:15:41 -0400

    final

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+
+
+var friendArray = [
+    {
+      Name: 'Joe',
+      level: 5,
+      time: 5,
+      snacks: 5,
+      caffiene: 5,
+      alcohol: 5,
+      fps: 5,
+      rpg: 5,
+      racing: 5,
+      action: 5,
+      fighting: 5
+    }
+  ];
+  
+  
+
+  module.exports = friendArray;
+  
---
+++ routing/apiRoutes.js
@@ -0,0 +1,17 @@
+var friendData = require('../app/data/friends.js');
+
+
+module.exports = function(app){
+    app.get("/api/friendList", function(req, res) {
+        res.json(friendData);
+      });
+
+      app.post("/api/friendList", function(req, res) {
+        friendData.push(req.body);
+      });
+    
+    
+    
+}
+
+
